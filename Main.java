26ad785e321c84f844812bdc6eb25d168a8a2d7f
HerculesAragao <manofhaf@gmail.com>
2024-03-11 19:29:16 -0300

    exemplo aula Padrões de projetos

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double a = leitor.nextDouble();
+        double b = leitor.nextDouble();
+        double c = leitor.nextDouble();
+        double media = ((a*2) + (b*3) + (c*5))/10;
+        System.out.println(String.format("MEDIA = %.1f" , media));
+    }
+}
